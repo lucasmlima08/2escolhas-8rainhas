4cd809bfb41677e7309f88d3311442acd4861faa
Lucas Myllenno Silva Monteiro Lima <lucas.mlima@hotmail.com.br>
2015-05-18 22:30:06 -0300

    Update Sound.java

--- app/src/main/java/rainhas/ia/uepb/com/rainhas/Sound.java
+++ app/src/main/java/rainhas/ia/uepb/com/rainhas/Sound.java
@@ -7,41 +7,6 @@ public class Sound {
 
     public static Context context;
 
-    // ------------------------------------------------------------------------------------------ //
-
-    /*public static MediaPlayer music;
-
-    public static void playBackgroundMusic(){
-        int audio = context.getResources().getIdentifier("musica_de_fundo", "raw", context.getPackageName());
-        music = MediaPlayer.create(context, audio);
-        music.setLooping(true);
-        music.start();
-    }
-
-    public static void pauseBackgroundMusic(){
-        if (music.isPlaying())
-            music.pause();
-    }
-
-    public static void unpauseBackgroundMusic(){
-        if (!music.isPlaying())
-            music.start();
-    }
-
-    public static void restartBackgroundMusic(){
-        music.stop();
-        music.setLooping(true);
-        music.start();
-    }
-
-    public static void stopBackgroundMusic(){
-        if (music.isPlaying()){
-            music.stop();
-        }
-    }*/
-
-    // ------------------------------------------------------------------------------------------ //
-
     public static MediaPlayer soundEffect;
 
     public static void playSoundEffect(String toque){
